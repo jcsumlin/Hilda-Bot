64a3c733b52f47df8f31fb403ccd478e67e18136
J_C___ <jcsumlin@pm.me>
2020-10-19 17:14:29 -0400

    Added new event migration

---
+++ alembic/versions/8ed6270aa028_add_new_event.py
@@ -0,0 +1,24 @@
+"""Add new Event
+
+Revision ID: 8ed6270aa028
+Revises: b6b7bdd7f9db
+Create Date: 2020-10-19 17:12:12.662067
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8ed6270aa028'
+down_revision = 'b6b7bdd7f9db'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
